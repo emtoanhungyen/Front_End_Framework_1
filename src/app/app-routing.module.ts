be66d630dc71da71cf329b007f1263954a33e392
toanpqph14805 <toanpqph14805@fpt.edu.vn>
2022-05-12 07:27:11 +0700

    feat: buổi học 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemoComponent } from './components/demo.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'demo', component: DemoComponent },
+  { path: 'table', component: PagesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from 'src/app/components/demo.component';
-import { DemocomponentComponent } from './components/democomponent/democomponent.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent, DemoComponent, DemocomponentComponent
+    AppComponent, DemoComponent, PagesComponent
   ],
   imports: [
     BrowserModule,
